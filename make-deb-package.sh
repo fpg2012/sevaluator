12e22d87e6c6ef2f519b9214c522e0d9e9ef8555
fpg2012 <fpg2012@foxmail.com>
2021-08-17 17:59:32 +0800

    add script to build deb

---
+++ make-deb-package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+make
+mkdir -p pkg-debian/DEBIAN
+mkdir -p pkg-debian/usr/include
+mkdir -p pkg-debian/usr/lib
+echo """Package: libsevaluator
+Version: 1.0.0
+Architecture: amd64
+Essential: no
+Section: utils
+Priority: optional
+Depends: libgmp10, libmpfr6
+Maintainer: nth233<fpg2012@yeah.net>
+Installed-Size: 1
+Description: simple/super expression evaluation library with big number support.""" > pkg-debian/DEBIAN/control
+cp *.so -t pkg-debian/usr/lib/
+cp *.h -t pkg-debian/usr/include/
+dpkg -b pkg-debian/ sevaluator_amd64.deb
+rm -r pkg-debian
+make clean
